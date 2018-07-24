3b5f07b11a9cfcbd8a3474db203e9ea10342fcf7
LaurenWorkosky <19lworkosky@student.dasd.org>
2018-07-24 18:54:15 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,28 @@
+puts "choose a number"
+number = gets.chomp.to_f
+
+divided_number = (number/3).to_f
+puts "#{divided_number}"
+rounded_number = divided_number.to_i
+puts "#{rounded_number}"
+final_number = rounded_number - divided_number
+puts "#{final_number}"
+
+dividednumber = number/5
+roundednumber = dividednumber.to_i
+finalnumber = roundednumber - dividednumber
+puts "#{finalnumber}"
+
+if final_number == 0 && finalnumber == 0
+  puts "FizzBuzz"
+
+  
+elsif final_number == 0 && finalnumber != 0
+  puts "Fizz"
+  
+else
+  puts "Buzz"
+  
+end
+  
+  
