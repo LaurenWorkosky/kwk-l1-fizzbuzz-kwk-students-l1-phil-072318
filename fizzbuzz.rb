ddf3b0665ae137660b7b085ef7bf43fd5879b9ac
LaurenWorkosky <19lworkosky@student.dasd.org>
2018-07-24 18:54:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@ puts "choose a number"
 number = gets.chomp.to_f
 
 divided_number = (number/3).to_f
-puts "#{divided_number}"
+
 rounded_number = divided_number.to_i
-puts "#{rounded_number}"
+
 final_number = rounded_number - divided_number
-puts "#{final_number}"
+
 
 dividednumber = number/5
 roundednumber = dividednumber.to_i
